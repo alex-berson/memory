14b6f00fbd46231d8724e3fb480793e1740c28ec
Alexander Berson <aberson@yahoo.com>
2020-09-24 09:19:51 +0200

    initial board design

---
+++ js/memory.js
@@ -0,0 +1,27 @@
+
+
+const boardSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--boardSize').replace(/[^0-9]/g,''))/100;
+
+console.log(parseInt(getComputedStyle(document.documentElement).getPropertyValue('--boardSize').replace(/[^0-9]/g,''))/100);
+
+if (window.innerHeight > window.innerWidth) {
+    document.documentElement.style.setProperty('--boardSize', 100/window.innerWidth * Math.ceil(window.innerWidth*boardSize/4)*4 + 'vmin');
+} else {
+    document.documentElement.style.setProperty('--boardSize', 100/window.innerHeight * Math.ceil(window.innerHeight*boardSize/4)*4 + 'vmin');
+}
+console.log(parseInt(getComputedStyle(document.documentElement).getPropertyValue('--boardSize').replace(/[^0-9]/g,''))/100);
+
+
+window.onload = function() {
+    document.fonts.ready.then(function() {
+
+        // function preventDefault(e){
+        //     e.preventDefault();
+        // }
+        
+        // document.body.addEventListener('touchmove', preventDefault, { passive: false });
+        // document.querySelector("body").style.transition = 'opacity 2s ease';
+    });
+    
+};
+
