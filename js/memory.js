a7f30018594badd831c7a0a30aa20b781195c849
Alexander Berson <aberson@yahoo.com>
2020-09-25 09:48:11 +0200

    aspect ratio

--- js/memory.js
+++ js/memory.js
@@ -11,11 +11,25 @@ if (window.innerHeight > window.innerWidth) {
 }
 console.log(parseInt(getComputedStyle(document.documentElement).getPropertyValue('--boardSize').replace(/[^0-9]/g,''))/100);
 
+
 console.log(navigator.language);
+console.log(screen.width/screen.height);
+console.log(outerHeight);
+console.log(innerHeight);
+
 
 window.onload = function() {
     document.fonts.ready.then(function() {
 
+        if (screen.width/screen.height > 0.5 && screen.width/screen.height < 2){
+            document.documentElement.style.setProperty('--maxSide', 6);
+            document.querySelector("#tile25").style.display = "none";
+            document.querySelector('#tile26').style.display = "none";
+            document.querySelector('#tile27').style.display = "none";
+            document.querySelector('#tile28').style.display = "none";
+        }
+            document.querySelector("#country1").innerHTML = outerHeight;
+            document.querySelector("#country2").innerHTML = innerHeight;
         // function preventDefault(e){
         //     e.preventDefault();
         // }
